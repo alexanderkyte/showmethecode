a45384177249ffbf03765e7932268beb51fa8417
sublimepua <alexanderkyte@gmail.com>
2012-12-11 00:05:32 -0500

    suddenly, rails

---
+++ webApp/showmethecode/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showmethecode::Application.config.secret_token = '1dbc67ba999353ef5c126b7800fc5d1b64b149c032edebebd210897bd0c878f529da02965bcc59bb70f3f8042ba789ba911b7c40c5ffa5c6ee71979244f6bcd1'
